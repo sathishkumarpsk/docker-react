5a97012d21cb05f6b61c25a551a5c478b3f2f2ba
Sathishkumar Periyasamy <sathishkumarpsk@comcast.com>
2021-06-20 17:38:35 +0530

    changed text for test run

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Mini React App - SathishPSK
+          Mini React App - SathishPSK Test
         </p>
         <a
           className="App-link"
